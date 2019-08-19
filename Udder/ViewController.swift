3bea45122c082a93e0edba7bc680576dcee17e92
Chris Marler <chris@tropicalsun.ca>
2019-08-19 11:09:03 -0600

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Chris Marler on 2019-08-19.
+//  Copyright © 2019 Chris Marler. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
