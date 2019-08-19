606811241be65f188e7d43629bb91deca3013fc7
Chris Marler <chris@tropicalsun.ca>
2019-08-19 13:20:49 -0600

    fixed it

--- Udder/ViewController.swift
+++ Udder/ViewController.swift
@@ -13,10 +13,10 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
+        print("hi mom")
         
     }
-    print ("hi mom")
+    
 
 }
 
